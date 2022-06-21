5016e07f2b0fadaf047be007f01a185d3c2d1797
gbyfns <101527892+gbyfns@users.noreply.github.com>
2022-06-22 09:38:18 +1200

    Add files via upload

---
+++ 00_base_v1.py
@@ -0,0 +1,23 @@
+'''
+program to calculate cost of serving sizes for recipes
+base component
+v1 - set up the skeleton of the program, test the functions are called as required
+GF 2022
+'''
+
+# import library ✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭
+
+# set up functions ✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭✭
+
+# function to calculate cost to make
+def cost_to_make():
+    print("cost to make")
+
+# function to calculate total cost of meal
+def cost_of_meal():
+    print("cost of meal")
+
+# function to calculate cost per serving
+def cost_per_serving():
+    print("cost per serving")
+
