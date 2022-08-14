9c47f6212c7f5b04a13e1f562e5fce0e0351d990
gbyfns <101527892+gbyfns@users.noreply.github.com>
2022-08-15 09:05:41 +1200

    Add files via upload

--- 00_base_v2.py
+++ 00_base_v2.py
@@ -2,6 +2,7 @@
 program to calculate cost of serving sizes for recipes
 base component
 v1 - set up the skeleton of the program, test the functions are called as required
+v2 - add not_blank function
 GF 2022
 '''
 
@@ -21,6 +22,9 @@ def not_blank(question, error_message):
             return response
         else:
             print(error_message)
+    return response
+
+
 
 # function to calculate cost to make
 def cost_to_make():
@@ -34,7 +38,31 @@ def cost_of_meal():
 def cost_per_serving():
     print("cost per serving")
 
-# main routine
-recipe_name = not_blank("Recipe name: ",
+# ✭✭✭✭✭✭✭✭✭✭✭ set up all lists and constants ✭✭✭✭✭✭✭✭✭✭✭
+
+thisdict =	{
+  "flour": 3.5,
+  "sugar": 3,
+  "milk": 2.5
+}
+x = thisdict["flour"]
+x = (125/1500)*x
+print("the cost of the flour will be ${:.2f}".format(x))
+
+# ✭✭✭✭✭✭✭✭✭✭✭ main routine ✭✭✭✭✭✭✭✭✭✭✭
+
+# introduction
+print("✭✭✭✭✭✭✭✭RECIPE COST PROGRAM✭✭✭✭✭✭✭✭")
+print("")
+print("Welcome to my program")
+
+print("")
+print("")
+
+
+# get recipe name
+recipe_name = not_blank("Please enter a recipe name: ",
                         "Sorry - this cant be blank, "
                         "Please enter your recipe name")
+print(recipe_name)
+
